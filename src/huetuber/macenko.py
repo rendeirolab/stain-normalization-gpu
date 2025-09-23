89fe0e41f60747eaf3f8017518150ccba08204df
sunagparasu <sunag022on@gmail.com>
2025-09-23 10:04:09 +0200

    added BaseStainNormalizer class

--- src/huetuber/macenko.py
+++ src/huetuber/macenko.py
@@ -1,9 +1,46 @@
 """CuPy implementation of Macenko stain normalization."""
 
-from huetuber.base import BaseStainNormalizer
+# from base import BaseStainNormalizer
+from abc import ABC, abstractmethod
 import cupy as cp
 import numpy as np
 # from kernel import covariance
+
+class BaseStainNormalizer(ABC):
+    """
+    Abstract base class for stain normalization algorithms.
+    """
+
+    def __call__(self, *args, **kwargs):
+        return self.normalize(*args, **kwargs)
+
+    @abstractmethod
+    def fit(self, target: cp.ndarray) -> None:
+        """
+        Fit the normalizer to the target image.
+
+        Parameters
+        ----------
+        target (ndarray) : Target image in RGB format. Could be any array type (e.g., NumPy, CuPy).
+
+        """
+        pass
+
+    @abstractmethod
+    def normalize(self, source: cp.ndarray) -> cp.ndarray:
+        """
+        Normalize the source image to match the target image.
+
+        Parameters
+        ----------
+        source (ndarray) : Source image in RGB format. Could be any array type (e.g., NumPy, CuPy).
+
+        Returns
+        -------
+        ndarray : Normalized image in RGB format. Same array type as input.
+        """
+        pass
+
 
 
 # class MacenkoNormalizer(BaseStainNormalizer):
